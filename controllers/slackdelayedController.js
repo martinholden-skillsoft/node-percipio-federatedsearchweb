bb90e86e29c824a2b45f17e7f30797153c121891
Martin Holden <martin.holden@skillsoft.com>
2019-06-05 14:38:40 +0100

    Update to use slack-block-kit

--- controllers/slackdelayedController.js
+++ controllers/slackdelayedController.js
@@ -1,29 +1,47 @@
 const axios = require('axios');
 const _ = require('lodash');
 const searchQuery = require('search-query-parser');
+const slackBlockKit = require('slack-block-kit');
 
-const slackBlockUtils = require('../lib/slackBlockUtils');
+// Slack Block Items
+const slackDivider = slackBlockKit.block.divider;
+const slackImage = slackBlockKit.block.image;
+const slackSection = slackBlockKit.block.section;
+const slackText = slackBlockKit.object.text;
+const slackMarkdownFormat = slackBlockKit.TEXT_FORMAT_MRKDWN;
+
 const stringUtils = require('../lib/stringUtils');
 
 const orgid = process.env.CUSTOMER_ORGID || null;
 const bearer = process.env.CUSTOMER_BEARER || null;
+const percipioSite = process.env.PERCIPIOSITE || null;
+const maxItems = process.env.MAX_ITEMS || 5;
 
-const buildMarkdown = value => {
+/**
+ * Get a MarkDown representation of a Percipio Item
+ *
+ * @param {object} percipioItem - The Percipio Item from the Content Discovery response
+ * @returns {string}
+ */
+const getPercipioItemMarkdown = percipioItem => {
   let courseInfo = null;
-  if (!_.isNull(value.associations.parent)) {
+  if (!_.isNull(percipioItem.associations.parent)) {
     courseInfo = [
       '*From course:*',
-      `* <${value.associations.parent.link}|${stringUtils.getPlainText(
-        value.associations.parent.title
+      `* <${percipioItem.associations.parent.link}|${stringUtils.getPlainText(
+        percipioItem.associations.parent.title
       )}>`
     ].join('\n');
   }
 
   let channelInfo = null;
-  if (!_.isNull(value.associations.channels) && !_.isEmpty(value.associations.channels)) {
+  if (
+    !_.isNull(percipioItem.associations.channels) &&
+    !_.isEmpty(percipioItem.associations.channels)
+  ) {
     const channelInfoArray = ['*From channel:*'];
 
-    _.forEach(value.associations.channels, channelValue => {
+    _.forEach(percipioItem.associations.channels, channelValue => {
       channelInfoArray.push(
         `* <${channelValue.link}|${stringUtils.getPlainText(channelValue.title)}>`
       );
@@ -32,16 +50,16 @@ const buildMarkdown = value => {
   }
 
   const result = [
-    `*<${value.link}|${stringUtils.getPlainText(value.localizedMetadata[0].title)}>*`,
-    `${value.contentType.displayLabel} ${stringUtils.getFriendlyDuration(
-      value.duration,
+    `*<${percipioItem.link}|${stringUtils.getPlainText(percipioItem.localizedMetadata[0].title)}>*`,
+    `${percipioItem.contentType.displayLabel} ${stringUtils.getFriendlyDuration(
+      percipioItem.duration,
       'minutes',
       true,
       '| '
     )}`,
     `${
-      !_.isNil(value.localizedMetadata[0].description)
-        ? _.truncate(stringUtils.getPlainText(value.localizedMetadata[0].description), {
+      !_.isNil(percipioItem.localizedMetadata[0].description)
+        ? _.truncate(stringUtils.getPlainText(percipioItem.localizedMetadata[0].description), {
             length: 1000,
             separator: ' '
           })
@@ -54,14 +72,27 @@ const buildMarkdown = value => {
   return result;
 };
 
-const createBlock = value => {
-  return slackBlockUtils.getSuccessBlock(
-    buildMarkdown(value),
-    value.imageUrl,
-    stringUtils.getPlainText(value.localizedMetadata[0].title)
-  );
+/**
+ * Get a Slack Block representation of a Percipio Item
+ *
+ * @param {object} percipioItem- The Percipio Item from the Content Discovery response
+ * @returns
+ * @returns {object}
+ */
+const getPercipioItemSlackBlock = percipioItem => {
+  return slackSection(slackText(getPercipioItemMarkdown(percipioItem), slackMarkdownFormat), {
+    accessory: slackImage(
+      percipioItem.imageUrl,
+      stringUtils.getPlainText(percipioItem.localizedMetadata[0].title)
+    )
+  });
 };
-
+/**
+ * Call the Percipio /content-discovery/v1/organizations/${orgid}/search-content
+ *
+ * @param {object} request
+ * @returns {object} Axios promise
+ */
 const getSearchResults = async request => {
   let requestParams = request || {};
 
@@ -89,114 +120,140 @@ const getSearchResults = async request => {
   return axios.request(axiosConfig);
 };
 
-const delayedResponse = async (request, modalityString, slackurl) => {
-  const { data } = await getSearchResults(request);
-
-  const blocks = [];
-
-  blocks.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: `These are top 5 things I found for you about *${request.q}*${modalityString}`
-    }
-  });
-
-  const divider = slackBlockUtils.getDividerBlock();
-
-  blocks.push(divider);
-
-  _.forEach(data, value => {
-    blocks.push(createBlock(value));
-    blocks.push(divider);
-  });
-
-  const result = {
-    response_type: 'ephemeral',
-    blocks
-  };
-
-  axios
-    .post(slackurl, result)
-    .then(res => {
-      console.log(`statusCode: ${res.statusCode}`);
-      console.log(res);
-    })
+/**
+ * Generates the Slack Delayed Response to post back to Slack
+ *
+ * @param {object} request - Percipio parameters for /search-content
+ * @param {*} slackResponseUrl - The slack url we will post our delayed response to.
+ */
+const generateSlackDelayedResponse = async (request, slackResponseUrl) => {
+  await getSearchResults(request)
     .catch(error => {
-      console.error(error);
+      // handle error
+      if (error.response) {
+        axios.post(slackResponseUrl, {
+          response_type: 'ephemeral',
+          blocks: [
+            slackDivider(),
+            slackSection(
+              slackText(
+                'Oops! Something went wrong when we tried to get the information for you',
+                slackMarkdownFormat
+              )
+            )
+          ]
+        });
+      }
+    })
+    .then(response => {
+      const blocks = [];
+      const totalRecords = parseInt(response.headers['x-total-count'], 10);
+
+      const percipioSearchUrl = new URL(percipioSite);
+      percipioSearchUrl.pathname = '/search';
+      percipioSearchUrl.search = `q=${request.q}`;
+      // Add a block with details of what we found
+      blocks.push(
+        slackSection(
+          slackText(
+            `These are top ${response.data.length} things I found for you about *${
+              request.q
+            }*${stringUtils.getModalityMarkdown(request)}`,
+            slackMarkdownFormat
+          )
+        )
+      );
+      // Add a block with link to the Percipio Site Search Results Page
+      blocks.push(
+        slackSection(
+          slackText(
+            `You can see all *${totalRecords}* items we found on *<${percipioSearchUrl}|${percipioSite}>*`,
+            slackMarkdownFormat
+          )
+        )
+      );
+      // Add a divider
+      blocks.push(slackDivider());
+      // For each item in the results add a Slack block
+      _.forEach(response.data, value => {
+        blocks.push(getPercipioItemSlackBlock(value));
+        blocks.push(slackDivider());
+      });
+
+      const result = {
+        response_type: 'ephemeral',
+        blocks
+      };
+
+      if (!_.isNil(slackResponseUrl)) {
+        axios.post(slackResponseUrl, result);
+      }
     });
 };
 
 const view = (slackReqObj, res) => {
-  let result;
   if (!slackReqObj.body) {
-    result = {
-      response_type: 'ephemeral',
-      text: '',
-      attachments: [slackBlockUtils.getErrorBlock(new Error('Invalid body'))]
-    };
-    res.json(result);
+    res
+      .json({
+        response_type: 'ephemeral',
+        blocks: slackSection(slackText('Invalid request', slackMarkdownFormat))
+      })
+      .end();
   }
 
   const options = { keywords: ['prefer'] };
   const searchQueryObj = searchQuery.parse(slackReqObj.body.text, options);
 
   const request = {
-    q: _.isObject(searchQueryObj) ? searchQueryObj.text : slackReqObj.body.text
+    q: _.isObject(searchQueryObj) ? searchQueryObj.text : slackReqObj.body.text,
+    max: maxItems
   };
 
   if (_.isEmpty(request.q)) {
-    result = {
-      response_type: 'ephemeral',
-      text: '',
-      attachments: [slackBlockUtils.getErrorBlock(new Error('Invalid query'))]
-    };
-    res.json(result);
+    res
+      .json({
+        response_type: 'ephemeral',
+        blocks: slackSection(slackText('Invalid query', slackMarkdownFormat))
+      })
+      .end();
   }
-
-  let modalityString = '';
 
   if (_.isObject(searchQueryObj) && searchQueryObj.prefer) {
     // READ, WATCH, LISTEN, PRACTICE
-
     switch (_.upperCase(searchQueryObj.prefer).substring(0, 4)) {
       case 'READ': // READ
         request.modality = 'READ';
-        modalityString = ' that you can *READ ABOUT*';
         break;
       case 'WATC': // WATCH
         request.modality = 'WATCH';
-        modalityString = ' that you can *WATCH*';
         break;
       case 'LIST': // LISTEN
         request.modality = 'LISTEN';
-        modalityString = ' that you can *LISTEN TO*';
         break;
       case 'PRAC': // PRACTICE
         request.modality = 'PRACTICE';
-        modalityString = ' that you can *PRACTICE WITH*';
         break;
       default:
         break;
     }
   }
 
-  const blocks = [];
-
-  blocks.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: `Let me see what I can find for you about *${request.q}*${modalityString}`
-    }
-  });
-
-  result = {
-    response_type: 'ephemeral',
-    blocks
-  };
-  delayedResponse(request, modalityString, slackReqObj.body.response_url);
-  res.json(result);
+  generateSlackDelayedResponse(request, slackReqObj.body.response_url);
+  res
+    .json({
+      response_type: 'ephemeral',
+      blocks: [
+        slackSection(
+          slackText(
+            `Let me see what I can find for you about *${
+              request.q
+            }*${stringUtils.getModalityMarkdown(request)}`,
+            slackMarkdownFormat
+          )
+        )
+      ]
+    })
+    .end();
 };
 
 module.exports = {
